eecf70290f7feaf149721b1c0fdff27b93ffedd2
toothlessdev <toothless042@gmail.com>
2024-06-04 06:00:27 +0900

    feat: Label, SearchBar, TextArea 컴포넌트 구현

--- src/components/forms/SearchBar/index.tsx
+++ src/components/forms/SearchBar/index.tsx
@@ -1,14 +1,15 @@
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Input } from "../Input";
+import { FormEventHandler } from "react";
 
 export const SearchBar = (props: React.ComponentProps<"input">) => {
     return (
-        <form className="flex flex-1 relative w-full max-w-md">
+        <div className="flex flex-1 relative w-full max-w-md">
             <div className="h-[40px] w-10 absolute flex items-center justify-center">
                 <FontAwesomeIcon icon={faSearch} />
             </div>
             <Input className="pl-8 w-full focus:outline-none focus:border-[#00a869] focus:border-2" {...props} />
-        </form>
+        </div>
     );
 };
