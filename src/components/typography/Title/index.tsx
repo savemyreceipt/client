9a7c53d1d27dfa14d89679ebfb6562bd47452ad4
toothlessdev <toothless042@gmail.com>
2024-06-08 03:16:38 +0900

    feat: Title, Paragraph 컴포넌트 구현

--- src/components/typography/Title/index.tsx
+++ src/components/typography/Title/index.tsx
@@ -7,7 +7,7 @@ export const Title: React.FC<ITitle> = ({ title, subtitle }) => {
     return (
         <div className="space-y-2 text-center">
             <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl">{title}</h1>
-            <p className="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">{subtitle}</p>
+            <p className="text-center mx-auto max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">{subtitle}</p>
         </div>
     );
 };
