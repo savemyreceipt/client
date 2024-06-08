3f53a0b44aeb88e1852e2322b974879993c22bac
toothlessdev <toothless042@gmail.com>
2024-06-09 08:09:26 +0900

    refactor: 디렉토리 구조 변경

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/forms/Button";
 import { useAuth } from "@/hooks/useAuth";
 import { faUser, faUserGear } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
