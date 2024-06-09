72d287d7106a5c27609c598db34c2396a66f99d3
toothlessdev <toothless042@gmail.com>
2024-06-10 00:17:14 +0900

    feat: TitleSkeleton 컴포넌트 구현

--- src/components/typography/Title/index.tsx
+++ src/components/typography/Title/index.tsx
@@ -7,7 +7,22 @@ export const Title: React.FC<ITitle> = ({ title, subtitle }) => {
     return (
         <div className="space-y-2 text-center">
             <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl">{title}</h1>
-            <p className="text-center mx-auto max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">{subtitle}</p>
+            <p className="text-center mx-auto max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                {subtitle}
+            </p>
+        </div>
+    );
+};
+
+export const TitleSkeleton = () => {
+    return (
+        <div className="space-y-2 text-center">
+            <div className="w-3/4 h-8 bg-gray-200 rounded mx-auto animate-pulse"></div>
+            <div className="mx-auto max-w-[900px] space-y-2">
+                <div className="w-full h-4 bg-gray-200 rounded animate-pulse"></div>
+                <div className="w-5/6 h-4 bg-gray-200 rounded animate-pulse"></div>
+                <div className="w-2/3 h-4 bg-gray-200 rounded animate-pulse"></div>
+            </div>
         </div>
     );
 };
