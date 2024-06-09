cfdd4fffc7185d5d9ca0de8cb7ffa37afbb23764
toothlessdev <toothless042@gmail.com>
2024-06-10 00:24:09 +0900

    refactor: RootContext, RootModal 로 병합

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,19 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider, useQueryClient } from "@tanstack/react-query";
-
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import "react-toastify/dist/ReactToastify.css";
-import { Toast } from "@/components/feedback/Toast/Toast";
-import { NavBar } from "@/components/navigation/NavBar";
-import { ProfileContextProvider } from "@/context/ProfileContext";
 import { useRouter } from "next/router";
+import "react-toastify/dist/ReactToastify.css";
 
 import { AnimatePresence } from "framer-motion";
 import { motion } from "framer-motion";
+
+import { RootModal } from "@/components/feedback/Modal";
+import { Toast } from "@/components/feedback/Toast/Toast";
+import { NavBar } from "@/components/navigation/NavBar";
+
+import "@/styles/globals.css";
+
+import { RootContextProvider } from "@/context/RootContext";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient({
     defaultOptions: {
@@ -30,13 +33,21 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
         <AnimatePresence mode="wait">
             <QueryClientProvider client={queryClient}>
-                <ProfileContextProvider>
+                <RootContextProvider>
+                    <RootModal />
                     <Toast />
+
                     <NavBar />
-                    <motion.main key={router.route} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
+                    <motion.main
+                        key={router.route}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.5 }}
+                    >
                         <Component {...pageProps} />
                     </motion.main>
-                </ProfileContextProvider>
+                </RootContextProvider>
             </QueryClientProvider>
         </AnimatePresence>
     );
