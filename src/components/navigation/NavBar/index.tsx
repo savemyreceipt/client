50bf190138436bce710e6a92cec8eb84390f657c
toothlessdev <toothless042@gmail.com>
2024-06-10 00:18:55 +0900

    fix: NavBar z-index 수정 및 NotificationDropdown 구현

--- src/components/navigation/NavBar/index.tsx
+++ src/components/navigation/NavBar/index.tsx
@@ -1,10 +1,24 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/display/DropDown";
-import { Button } from "../../forms/Button";
-import { navDropDownItems, navItems } from "@/constants/navItems";
-import { useAuth } from "@/hooks/useAuth";
 import { useRouter } from "next/router";
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/display/DropDown";
+import { NotificationDropDown } from "@/components/display/DropDown/NotificationDropDown";
+
+import { useAuth } from "@/hooks/member/useAuth";
+
+import { BellIcon } from "@/assets/BellIcon";
+
+import { navDropDownItems, navItems } from "@/constants/navItems";
+
+import { Button } from "../../forms/Button";
 import { useProfile } from "@/context/ProfileContext";
 
 export const NavBar = () => {
@@ -14,8 +28,8 @@ export const NavBar = () => {
     const profile = useProfile();
 
     return (
-        <header className="fixed top-0 left-0 w-screen flex items-center h-16 px-4 border-b shrink-0 bg-white md:px-6">
-            <Link className="flex items-center gap-2 text-lg font-semibold md:text-base" href="#">
+        <header className="z-40 fixed top-0 left-0 w-screen flex items-center h-16 px-4 border-b shrink-0 bg-white md:px-6">
+            <Link className="flex items-center gap-2 text-lg font-semibold md:text-base" href="/groups">
                 Save My Receipt
             </Link>
 
@@ -29,7 +43,18 @@ export const NavBar = () => {
                 })}
             </nav>
 
-            <div className="ml-auto flex items-center gap-2">
+            <div className="flex items-center gap-4 ml-auto mr-5">
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" size="icon" className="rounded-full">
+                            <BellIcon className="w-5 h-5 text-gray-900" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <NotificationDropDown />
+                </DropdownMenu>
+            </div>
+
+            <div className="flex items-center gap-2">
                 <DropdownMenu>
                     <DropdownMenuTrigger asChild>
                         <Button className="rounded-full" size="icon" variant="ghost">
@@ -57,7 +82,8 @@ export const NavBar = () => {
                                     key={navDropDownItem.id}
                                     onClick={() => {
                                         router.push(navDropDownItem.href);
-                                    }}>
+                                    }}
+                                >
                                     {navDropDownItem.text}
                                 </DropdownMenuItem>
                             );
