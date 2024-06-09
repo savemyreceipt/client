1c37e7a1329bef2854c7039d735e04abc2ab3cce
toothlessdev <toothless042@gmail.com>
2024-06-10 00:22:10 +0900

    feat: useNewGroup 훅 구현

---
+++ src/hooks/groups/useNewGroup.ts
@@ -0,0 +1,37 @@
+import { useRouter } from "next/router";
+import { useCallback, useRef } from "react";
+
+import { queryClient } from "@/pages/_app";
+
+import { groupsService } from "@/services/groups/groups.service";
+
+import { useMutation } from "@tanstack/react-query";
+
+export const useNewGroup = () => {
+    const nameRef = useRef<HTMLInputElement>(null);
+    const cityRef = useRef<HTMLInputElement>(null);
+    const organizationRef = useRef<HTMLInputElement>(null);
+    const descriptionRef = useRef<HTMLTextAreaElement>(null);
+
+    const router = useRouter();
+
+    const { mutate } = useMutation({
+        mutationFn: () =>
+            groupsService.createGroup({
+                name: nameRef.current?.value as string,
+                city: cityRef.current?.value as string,
+                organization: organizationRef.current?.value as string,
+                description: descriptionRef.current?.value as string,
+            }),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: [`/groups`] });
+            router.replace("/groups");
+        },
+    });
+
+    const handleSubmit = useCallback(() => {
+        mutate();
+    }, [mutate]);
+
+    return { nameRef, cityRef, organizationRef, descriptionRef, handleSubmit };
+};
