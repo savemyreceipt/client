d9ba3f40a0cce74c28a4cb80a17c054592aac1f0
toothlessdev <toothless042@gmail.com>
2024-06-10 00:15:08 +0900

    feat: 영수증 이미지 업로드 useReceiptUpload 훅 구현

---
+++ src/hooks/receipt/useReceiptUpload.ts
@@ -0,0 +1,100 @@
+import { useRouter } from "next/router";
+import { ChangeEvent, FormEvent, useCallback, useRef, useState } from "react";
+import { toast } from "react-toastify";
+
+import { receiptService } from "@/services/receipt/receipt.service";
+import { IUploadReceiptResponse } from "@/services/receipt/receipt.types";
+
+import { useUploadModal } from "@/context/UploadModalContext";
+
+export const useReceiptUpload = (maxFileSize: number = 5 * 1024 * 1024) => {
+    const router = useRouter();
+    const { setIsModalOpened } = useUploadModal();
+
+    const categoryRef = useRef<HTMLInputElement>(null);
+    const descriptionRef = useRef<HTMLInputElement>(null);
+    const purchaseDateRef = useRef<HTMLInputElement>(null);
+    const priceRef = useRef<HTMLInputElement>(null);
+
+    const imageInput = useRef<HTMLInputElement>(null);
+    const [file, setFile] = useState<File | null>(null);
+    const [filePreview, setFilePreview] = useState<string | ArrayBuffer | null>(null);
+
+    const [isUploading, setIsUploading] = useState<boolean>(false);
+    const [data, setData] = useState<IUploadReceiptResponse["data"] | null>(null);
+
+    const handleFileChange = useCallback(
+        (e: ChangeEvent<HTMLInputElement>) => {
+            const file = e.target.files as FileList;
+
+            if (file[0].size > maxFileSize) {
+                toast.error(`이미지 파일의 크기는 ${maxFileSize / (1024 * 1024)} MB 이하여야 합니다`);
+                return;
+            }
+            setFile(file[0]);
+
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setFilePreview(reader.result);
+                const request = receiptService.uploadReceipt(Number(router.query.id), file[0] as File);
+                setIsUploading(true);
+                toast
+                    .promise(request, {
+                        success: "영수증 분석 성공!",
+                        pending: "영수증 분석 중입니다..",
+                        error: "이미지 분석에 실패하였습니다",
+                    })
+                    .then((data) => {
+                        if (
+                            !(
+                                categoryRef.current &&
+                                descriptionRef.current &&
+                                purchaseDateRef.current &&
+                                priceRef.current
+                            )
+                        )
+                            return;
+
+                        categoryRef.current.value = data.category;
+                        descriptionRef.current.value = data.description;
+                        purchaseDateRef.current.value = data.purchaseDate;
+                        priceRef.current.value = data.price.toString();
+                    })
+                    .finally(() => {
+                        setIsUploading(false);
+                    });
+            };
+            reader.readAsDataURL(file[0] as Blob);
+        },
+        [maxFileSize, router.query.id],
+    );
+
+    const handleFileUpload = useCallback(() => {
+        imageInput.current?.click();
+    }, []);
+
+    const handleSubmit = useCallback(async (e: FormEvent) => {
+        e.preventDefault();
+    }, []);
+
+    const handleCancel = useCallback(async () => {
+        if (isUploading) return;
+        receiptService.deleteReceipt(data?.id as number).then(() => {
+            setIsModalOpened(false);
+        });
+    }, [setIsModalOpened, isUploading, data?.id]);
+
+    return {
+        priceRef,
+        categoryRef,
+        descriptionRef,
+        purchaseDateRef,
+        imageInput,
+        file,
+        filePreview,
+        handleFileChange,
+        handleFileUpload,
+        handleSubmit,
+        handleCancel,
+    };
+};
