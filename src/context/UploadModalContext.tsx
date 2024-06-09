ac69a779180817863e13a6ae7160f600a08fb7a6
toothlessdev <toothless042@gmail.com>
2024-06-10 00:18:18 +0900

    feat: UploadModalContext 및 useUploadModal 훅 구현

---
+++ src/context/UploadModalContext.tsx
@@ -0,0 +1,21 @@
+import { Dispatch, SetStateAction, createContext, useContext, useState } from "react";
+
+export const UploadModalContext = createContext<{
+    isModalOpened: boolean;
+    setIsModalOpened: Dispatch<SetStateAction<boolean>>;
+} | null>(null);
+
+export const UploadModalContextProvider = ({ children }: { children: React.ReactNode }) => {
+    const [isModalOpened, setIsModalOpened] = useState<boolean>(false);
+    return (
+        <UploadModalContext.Provider value={{ isModalOpened, setIsModalOpened }}>
+            {children}
+        </UploadModalContext.Provider>
+    );
+};
+
+export const useUploadModal = () => {
+    const context = useContext(UploadModalContext);
+    if (!context) throw new Error("useModal 은 UploadModalContext 내부에서 사용되어야 합니다");
+    return context;
+};
