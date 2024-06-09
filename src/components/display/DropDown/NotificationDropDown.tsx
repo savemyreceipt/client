13734c6d734aac5085f76dade15bcecafa5ae4a5
toothlessdev <toothless042@gmail.com>
2024-06-10 02:55:27 +0900

    feat: NotificationDropDown 컴포넌트 구현

--- src/components/display/DropDown/NotificationDropDown.tsx
+++ src/components/display/DropDown/NotificationDropDown.tsx
@@ -1,18 +1,43 @@
 import { DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator } from ".";
 
+import { useNotifications } from "@/hooks/notification/useNotifications";
+
+export interface INotification {
+    title: string;
+    message: string;
+}
+
 export const NotificationDropDown = () => {
+    const { isPending, data } = useNotifications();
+
     return (
         <DropdownMenuContent align="end" className="w-[320px] h-[400px] overflow-y-scroll">
             <DropdownMenuLabel>Notifications</DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuItem>
-                <div className="flex items-center justify-between">
-                    <div className="flex items-center gap-2">
-                        <p className="text-sm font-medium text-gray-900 ">New receipt added</p>
-                        <p className="text-sm text-gray-500 ">John Doe added a new receipt</p>
-                    </div>
+                <div className="flex flex-col items-center justify-between">
+                    {!isPending &&
+                        data &&
+                        data?.map((notification) => {
+                            return (
+                                <NotificationItem
+                                    key={notification.id}
+                                    title={notification.title}
+                                    message={notification.message}
+                                />
+                            );
+                        })}
                 </div>
             </DropdownMenuItem>
         </DropdownMenuContent>
+    );
+};
+
+export const NotificationItem: React.FC<INotification> = ({ title, message }) => {
+    return (
+        <div className="block w-full my-1">
+            <p className="text-sm font-medium text-gray-900 ">{title}</p>
+            <p className="text-sm text-gray-500 overflow-hidden whitespace-nowrap text-ellipsis">{message}</p>
+        </div>
     );
 };
