69afb97b9723502224181b5b96f0e44bd4e8c7f3
toothlessdev <toothless042@gmail.com>
2024-06-10 00:17:36 +0900

    feat: useAuth 훅 구현

---
+++ src/hooks/member/useAuth.ts
@@ -0,0 +1,32 @@
+import { getToken, removeToken, setToken } from "@/utils/token";
+import { useRouter } from "next/router";
+import { useCallback, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
+export const OAUTH_REDIRECT_URL = process.env.NEXT_PUBLIC_OAUTH_REDIRECT_URL as string;
+
+export const useAuth = () => {
+    const router = useRouter();
+
+    const token = getToken();
+    const isAuthenticated = token !== null;
+
+    const signIn = useCallback(() => {
+        router.push("/groups");
+    }, [router]);
+
+    const signOut = useCallback(() => {
+        removeToken();
+        router.replace("/");
+        toast.success("로그아웃 완료!");
+    }, [router]);
+
+    useEffect(() => {
+        if (router.pathname === "/login") {
+            const token = router.query.accessToken as string;
+            setToken(token);
+        }
+    }, [router]);
+
+    return { isAuthenticated, signIn, signOut, token };
+};
