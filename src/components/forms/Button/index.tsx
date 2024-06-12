13fef1a94a996fa0f2bfb2e06feaf9bf511a7ce2
toothlessdev <toothless042@gmail.com>
2024-06-12 20:29:13 +0900

    refactor: Button 컴포넌트 리팩토링

--- src/components/forms/Button/index.tsx
+++ src/components/forms/Button/index.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
+
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/styles/utils";
+
+import { Slot } from "@radix-ui/react-slot";
 
 const buttonVariants = cva(
     "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#00A869] focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
@@ -28,17 +30,21 @@ const buttonVariants = cva(
             variant: "default",
             size: "default",
         },
-    }
+    },
 );
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
+export interface ButtonProps
+    extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+        VariantProps<typeof buttonVariants> {
     asChild?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button";
-    return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
-});
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+    ({ className, variant, size, asChild = false, ...props }, ref) => {
+        const Comp = asChild ? Slot : "button";
+        return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
+    },
+);
 Button.displayName = "Button";
 
 export { Button, buttonVariants };
