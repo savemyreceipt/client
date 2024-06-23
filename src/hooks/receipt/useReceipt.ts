1c2ae23899e6da8da09e184cc58eb1c37bbc4517
toothlessdev <toothless042@gmail.com>
2024-06-23 18:23:08 +0900

    feat: useReceiptDetail 훅 구현

--- src/hooks/receipt/useReceipt.ts
+++ src/hooks/receipt/useReceipt.ts
@@ -1,7 +1,9 @@
 import { useRouter } from "next/router";
 
 import { groupsService } from "@/services/groups/groups.service";
+import { receiptService } from "@/services/receipt/receipt.service";
 
+import { useDetailModal } from "@/context/DetailModalContext";
 import { useQuery } from "@tanstack/react-query";
 
 export const useReceipt = (page: number = 0) => {
@@ -9,6 +11,17 @@ export const useReceipt = (page: number = 0) => {
     const query = useQuery({
         queryKey: [`/groups/:groupId/receipts`, `/groups/${Number(router.query.id)}`],
         queryFn: () => groupsService.readReceiptsByGroupId(Number(router.query.id), page),
+    });
+
+    return { ...query };
+};
+
+export const useReceiptDetail = () => {
+    const { receiptId } = useDetailModal();
+
+    const query = useQuery({
+        queryKey: [`/receipts/${receiptId}`, `/receipts`],
+        queryFn: () => receiptService.readReceiptById(receiptId),
     });
 
     return { ...query };
